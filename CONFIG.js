eeb9826ead0fdd142a0bc83fbf6c00b720c20bf9
priyans11 <priyanshu82711@gmail.com>
2025-04-16 21:45:29 +0530

    Bored

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Hi there, I have something to say. </br> Will you be my girlfriend?",
+  descriptionHeader:
+    "Please answer honestly. If you leave without answering, it means you've said YES.",
+  buttonYes: "Yes, I will",
+  buttonNo: "No, I won't",
+  titleModar: "Thank you.",
+  descriptionModar:
+    "Thank you for giving me a chance to be by your side, to protect and care for you. </br> I may not not be the most handsome, nor the most talented, but I have a sincere heart. You are my whole world. </br> I'll pick you up tonight. Please dress up nicely! There will be a surprise waiting for you.</br> Love you <3",
+};
